1065b200e98181dd6e6b5c858f4694f469047a6a
Zawent <100706312+Zawent@users.noreply.github.com>
2024-02-01 15:29:09 -0500

    Add files via upload

---
+++ procrastinator_back/database/migrations/2023_11_14_093542_create_encuesta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEncuestaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('encuestas', function (Blueprint $table) {
+            $table->id();
+            $table->string('pregunta1');
+            $table->string('pregunta2');
+            $table->string('pregunta3');
+            $table->string('pregunta4');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encuestas');
+    }
+}
