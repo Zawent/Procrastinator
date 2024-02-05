8d6e8224fb61f06a51f618722edfe51133729712
bebelinn <145699248+bebelinn@users.noreply.github.com>
2024-02-05 07:52:05 -0500

    ganar comodines

--- procrastinator_back/app/Http/Controllers/API/ComodinApiController.php
+++ procrastinator_back/app/Http/Controllers/API/ComodinApiController.php
@@ -74,10 +74,15 @@ class ComodinApiController extends Controller
         if ($comodin) {
             return response()->json(['mensaje' => 'No tienes comodines disponibles'], 404);
         } 
-            
+          $tiempoTranscurrido = now()->diffInHours($comodin->tiempo_generacion);
+          if ($tiempoTranscurrido >= 50) {
+            $comodin->update(['tiempo_generacion' => now()]); 
+            return response()->json(['mensaje' => 'Comodín ganado con éxito']); 
+        } else {
+            return response()->json(['mensaje' => 'Aún no han pasado 50 horas desde el último comodín']);
         }
-
     }
+}
+    
 
-  
 
