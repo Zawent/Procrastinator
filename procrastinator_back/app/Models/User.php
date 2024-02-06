208bdfd25f25d6f4a8a458aa5f8a6d976db8e6db
Heidy13 <h.andreaortiz1325@gmail.com>
2024-02-06 10:41:31 -0500

    edad ya no, es fecha ahora

--- procrastinator_back/app/Models/User.php
+++ procrastinator_back/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'edad', 'ocupacion', 'email', 'password', 'id_rol',
+        'name', 'fecha_nacimiento', 'ocupacion', 'email', 'password', 'id_rol',
     ];
 
     /**
