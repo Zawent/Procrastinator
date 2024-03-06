49c7ad8775fe6a64ae2d8c902e4e567c2ea3e9d1
Zawent <fabianvargas1099@gmail.com>
2024-03-06 07:53:53 -0500

    Ultimo back en el cual sirve TODA LA APP SIIIIIIIIIIIIIIIIII HPTAAAAAAA

--- procrastinator_back/routes/api.php
+++ procrastinator_back/routes/api.php
@@ -40,7 +40,7 @@ Route::get('comodines/cantidad/{id_user}', [ComodinApiController::class, 'cantid
 Route::get('consejo/diario/{id}',[ConsejoApiController::class, 'consejoDiario']);
 Route::get('consejos/{id}',[ConsejoApiController::class, 'consejosPorId']);
 Route::patch('/desactivar-bloqueo/{id}', [BloqueoApiController::class, 'update']);
-Route::get('tener-bloqueo', [BloqueoApiController::class, 'getBloqueo']);
+Route::get('tener-bloqueo', [BloqueoApiController::class, 'getBloqueo'])->middleware("auth:api");
 Route::post('apps/{id_user}',[AppApiController::class, 'listarPorUser']);
 Route::patch('desactivado', [BloqueoApiController::class, 'marcarDesbloqueado']);
 
