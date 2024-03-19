1b490a4d6919c1f38e62161189ddbf67b16d685f
Heidy13 <h.andreaortiz1325@gmail.com>
2024-03-19 07:27:00 -0500

    semillas

--- procrastinator_back/database/seeders/UserSeeder.php
+++ procrastinator_back/database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create(['id'=>1, 'name'=>'Administrador', 'fecha_nacimiento'=>'1999-02-10', 'ocupacion'=>'estudiante', 'email'=>'procras03@gmail.com', 'password'=>bcrypt('12345678'), 'id_rol'=>1, 'email_verified_at'=>'1999-02-10 10:00:00']);
-        User::create(['id'=>2, 'name'=>'user', 'fecha_nacimiento'=>'1999-02-10', 'ocupacion'=>'estudiante', 'email'=>'prueba@gmail.com', 'password'=>bcrypt('12345678'), 'id_rol'=>2, 'nivel_id'=>1]);
-        User::create(['id'=>3, 'name'=>'user', 'fecha_nacimiento'=>'1999-02-10', 'ocupacion'=>'estudiante', 'email'=>'aa@aa', 'password'=>bcrypt('aaaa'), 'id_rol'=>2, 'nivel_id'=>1]);
+        User::create(['id'=>2, 'name'=>'user', 'fecha_nacimiento'=>'1999-02-10', 'ocupacion'=>'estudiante', 'email'=>'prueba@gmail.com', 'password'=>bcrypt('12345678'), 'id_rol'=>2, 'nivel_id'=>1, 'email_verified_at'=>'1999-02-10 10:00:00']);
+        User::create(['id'=>3, 'name'=>'user', 'fecha_nacimiento'=>'1999-02-10', 'ocupacion'=>'estudiante', 'email'=>'aa@aa', 'password'=>bcrypt('aaaa'), 'id_rol'=>2, 'nivel_id'=>1, 'email_verified_at'=>'1999-02-10 10:00:00']);
     }
 }
